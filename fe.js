f39a5692f3f2ac14440a5e4aabef8f9839eb78ec
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-07-04 16:22:29 +0200

    Update fe.js

--- fe.js
+++ fe.js
@@ -5,7 +5,7 @@ desc:'A big mod that add a lot of various things',
 engineVersion:1,
 manifest:'https://rawgit.com/OscarHavril/futuristic-enginering/master/feManifest.js'
 requires:['Default dataset*'],
-sheets:{'cookieSheet':'https://raw.githubusercontent.com/Generatoror/hello-world/master/sprits.png'},//custom stylesheet (note : broken in IE and Edge for the time being) Note 2: Edit this too !
+sheets:{'feSheet':'https://raw.githubusercontent.com/Generatoror/hello-world/master/sprits.png'},//custom stylesheet (note : broken in IE and Edge for the time being) Note 2: Edit this too !
 func:function()
 {
 new G.resCategories={
@@ -18,7 +18,7 @@ new G.resCategories={
 new G.Tech({
         name:'[FE]Animals',
         desc:'@unlocks a new resource category: [animals], which can be stored with [farm]s',
-        icon:[0,1,'cookieSheet'], //Edit
+        icon:[0,1,'feSheet'], //Edit
         cost:{'insight':15},
         req:{'hunting':true},
     });
@@ -26,7 +26,7 @@ new G.Tech({
 new G.Tech({
         name:'[FE]Farming',
         desc:'@unlocks [farm]s, wich breeds some animals for food',
-        icon:[0,1,'cookieSheet'], //Edit
+        icon:[0,1,'feSheet'], //Edit
         cost:{'insight':15},
         req:{'cooking':true},
     });
