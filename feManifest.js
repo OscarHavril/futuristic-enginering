d5716076d2edace3c928715206223c5a4f67cc5c
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-07-04 16:46:07 +0200

    Update feManifest.js

--- feManifest.js
+++ feManifest.js
@@ -2,8 +2,5 @@ G.DeclareManifest({
 name:'Futuristic Enginering manifest',
 updates:{
 	'Futuristic Enginering*':'https://rawgit.com/OscarHavril/futuristic-enginering/master/fe.js',
-	'Example mod*':'mod.js',//we're updating any mod with a name that begins with "Example mod"
-	'Test mod*':'https://rawgit.com/Generatoror/hello-world/master/cookieLegacy.js',
-	'Thot mod*':'https://rawgit.com/torcado194/neverending-legacy-mods/master/thot.js'
 }
 });
