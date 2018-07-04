35d02cdd273c854a64308ba45bb55b79bda5e307
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-07-04 15:08:20 +0200

    Update feManifest.js

--- feManifest.js
+++ feManifest.js
@@ -1,6 +1,6 @@
 G.DeclareManifest({
 name:'Futuristic Enginering manifest',
 updates:{
-	'Futuristic Enginering*':'',
+	'Futuristic Enginering*':'https://raw.githubusercontent.com/OscarHavril/futuristic-enginering/master/fe.js',
 }
 });
