f6acead7abc4b851280f2480f43f7dc697dcdf29
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-07-04 14:56:00 +0200

    Create fe.js

---
+++ fe.js
@@ -0,0 +1,32 @@
+G.AddData({
+name:'Futuristic Enginering',
+author:'OscarHavril ',
+desc:'A big mod that add a lot of various things',
+engineVersion:1,
+manifest:'https://rawgit.com/OscarHavril/futuristic-enginering/master/feManifest.js'
+requires:['Default dataset*'],
+sheets:{'cookieSheet':'https://raw.githubusercontent.com/Generatoror/hello-world/master/sprits.png'},//custom stylesheet (note : broken in IE and Edge for the time being) Note 2: Edit this too !
+func:function()
+{
+new G.resCategories={
+        'animals':{
+            name:'Animals',
+            base:[],
+            side:['population','worker','happiness','health','land','coin'],
+        },
+ 
+new G.Tech({
+        name:'[FE]Animals',
+        desc:'@unlocks a new resource category: [animals], which can be stored with [farm]s',
+        icon:[0,1,'cookieSheet'], //Edit
+        cost:{'insight':15},
+        req:{'hunting':true},
+    });
+ 
+new G.Tech({
+        name:'[FE]Farming',
+        desc:'@unlocks [farm]s, wich breeds some animals for food',
+        icon:[0,1,'cookieSheet'], //Edit
+        cost:{'insight':15},
+        req:{'cooking':true},
+    });
